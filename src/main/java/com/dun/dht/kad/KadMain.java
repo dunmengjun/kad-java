3bba43108561d2161270327f8d09d46e09edd7ae
Mengjun Dun <mjdun@thoughtworks.com>
2018-05-21 16:04:07 +0800

    Initial commit

---
+++ src/main/java/com/dun/dht/kad/KadMain.java
@@ -0,0 +1,8 @@
+package com.dun.dht.kad;
+
+public class KadMain {
+
+    public static void main(String[] args) {
+        System.out.println("kad-java...");
+    }
+}
---
+++ src/test/java/com/dun/dht/kad/KadMainTest.java
@@ -0,0 +1,8 @@
+package com.dun.dht.kad;
+
+
+import static org.junit.Assert.*;
+
+public class KadMainTest {
+
+}
